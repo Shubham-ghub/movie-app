c9413b72dcb2d27ab67f89d0052de3a75e28ef0b
Shubham Dungarwal <dungarwalshubham56@gmail.com>
2025-09-29 13:09:22 +0530

    Update movieService.js

--- src/features/movies/movieService.js
+++ src/features/movies/movieService.js
@@ -1,40 +1,14 @@
 import axios from "axios";
 
 const fetchMovie = async () => {
-  const queries = [
-      "avengers",
-      "iron man",
-    "batman",
-    "mission impossible",
-    "superman",
-    "harry potter",
-    "spiderman",
-    "thor",
-    "hulk",
-    "matrix",
-    "joker",
-    "wonder woman",
-  ];
-
-
-  const requests = queries.map((q) =>
-    axios.get(`http://www.omdbapi.com/?s=${q}&apikey=8099f99c&type=movie&page=1`)
-  );
-
-  const responses = await Promise.all(requests);
-
   
-  const movies = responses
-    .map((res) => (res.data && res.data.Response === "True" ? res.data.Search : []))
-    .flat()
-
-  return movies
+  const response = await axios.get("https://api.imdbapi.dev/titles")
+  return response.data.titles
 };
 
 
 const singleMovie = async(id)=>{
     const response = await axios.get(`http://www.omdbapi.com/?t=${id}&apikey=8099f99c`)
-    
     return response.data
     
 }
@@ -42,9 +16,11 @@ const singleMovie = async(id)=>{
 
 export const searchMovie = async(query)=>{
   const response = await axios.get(`http://www.omdbapi.com/?s=${query}&apikey=8099f99c`)
-  console.log(response.data);
   return response.data
   
 
 }
+
+
+
 export const movieService = { fetchMovie , singleMovie  }
